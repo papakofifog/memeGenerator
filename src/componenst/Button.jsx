93433eff56c49346708190a053550f9f5653fac7
papakofifog <paakof275@gmail.com>
2023-03-06 20:44:55 +0000

    new changes

--- src/componenst/Button.jsx
+++ src/componenst/Button.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Button(props){
     return (
-        <button className="btn memebtn">
+        <button className="btn memebtn" onClick={props.onClick}>
            {props.title} { props.image && <span>{props.image}</span>}
         </button>
 
--- src/componenst/memeForm.jsx
+++ src/componenst/memeForm.jsx
@@ -2,7 +2,19 @@ import React from "react";
 import Input from "./Input";
 import Button from "./Button";
 import Image from "./Image";
+import memesData from "../assets/memesData";
+
 export default function MemeForm(){
+    function newRandomNumber(){
+        return Math.floor(Math.random()* (memesData.data.memes.length-0+1)) + 0;
+    }
+
+    function handleMemeSelection(){
+        let randomNumber= newRandomNumber();
+        let randomMeme=memesData.data.memes[randomNumber];
+        console.log(randomMeme);
+    }
+
     return (
         <div className="memeForm">
             <div className="flex inputsFlex">
@@ -10,7 +22,9 @@ export default function MemeForm(){
                 <Input type="text" placeholder="and take money" />
             </div>
             <div className="flex-center">
-                <Button title="Get a new meme image" />
+                <Button title="Get a new meme image" 
+                onClick={handleMemeSelection}
+                />
             </div>
             <Image />
             
